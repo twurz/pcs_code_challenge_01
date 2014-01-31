d9d963525e2b292f4f930bf12ff20415a3ffb8fc
twurz <travis.wurzer@yahoo.com>
2014-01-30 16:56:45 -0800

    added increment 4 and to the README.md

--- madlibs.rb
+++ madlibs.rb
@@ -1,50 +1,62 @@
 require 'pry'
 
-def prompt
+class UI
 
-	words = Hash.new
+  def prompt
 
-	puts "Please enter a noun."
-	words[:noun] = gets.chomp
+    words = Hash.new
 
-	puts "Please enter an adjective."
-	words[:adjective] = gets.chomp
+    puts "Please enter a noun."
+    words[:noun] = gets.chomp
 
-	puts "Please enter a Verb."
-	words[:verb] = gets.chomp
+    puts "Please enter an adjective."
+    words[:adjective] = gets.chomp
 
-	puts "Please enter an Adverb."
-	words[:adverb] = gets.chomp
+    puts "Please enter a Verb."
+    words[:verb] = gets.chomp
 
-	puts "Please enter an Object."
-	words[:object] = gets.chomp
+    puts "Please enter an Adverb."
+    words[:adverb] = gets.chomp
 
-	return words
+    puts "Please enter an Object."
+    words[:object] = gets.chomp
+
+    return words
+
+  end
+end
+
+class Sentence_Template
+  attr_accessor :template
+  def initialize (template_string)
+    @template = template_string
+  end
+  def insert (parts_of_speech)
+    sentence = @template
+    parts_of_speech.each do |part, word|
+      regex = "\(#{part.to_s}\)"
+      sentence.sub!(regex, word)
+    end
+    sentence
+  end
 end
 
 
+    ui = UI.new
+
+    input_words = ui.prompt
 
 
-output = prompt
+    templates = Array.new
 
-binding.pry #execution will drop into pry at this line
+    templates[0] = Sentence_Template.new("The (adjective) (noun) suddenly and (adverb) had to (verb) the (object).") 
+    templates[1] = Sentence_Template.new("The (noun) (verb).")
+    # templates[0] = "The #{output[:adjective]} #{output[:noun]} suddenly and #{output[:adverb]} had to #{output[:verb]} the #{output[:object]}."
+    # templates[1] = "Your #{output[:noun]} is very #{output[:adjective]}."
+    # templates[2] = "While in the #{output[:noun]}, I saw #{output[:object]} #{output[:verb]}."
+  templates.each do |sentence_template|
+    puts sentence_template.template
+    puts sentence_template.insert input_words
+  end
 
-class Sentence_template
-end
-
-templates = Array.new
-
-	templates[0] = "The #{output[:adjective]} #{output[:noun]} suddenly and #{output[:adverb]} had to #{output[:verb]} the #{output[:object]}."
-	#binding.pry
-	templates[1] = "Your #{output[:noun]} is very #{output[:adjective]}."
-	#binding.pry
-	templates[2] = "While in the #{output[:noun]}, I saw #{output[:object]} #{output[:verb]}."
-	#binding.pry
-
-puts templates
-
-
-
-exit
-
-
+# puts templates
